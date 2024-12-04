5ef5c91feee5b39d63f79a8ce7ba89544fa9a1c2
MoradFN <71790104+MoradFN@users.noreply.github.com>
2024-12-04 19:54:07 +0100

    fix environment for hosting, set up cors.

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
 import express from "express";
 
+import dotenv from "dotenv";
+
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
@@ -9,11 +11,36 @@ import testRoute from "./routes/testRoute.js";
 import authRoute from "./routes/authRoute.js";
 import restaurantRoute from "./routes/restaurantRoute.js";
 
+dotenv.config();
+
 const app = express();
+
+// Environment-specific configurations för cors, anslutning mellan frontend och backend.
+const allowedOrigins = process.env.ALLOWED_ORIGINS
+  ? process.env.ALLOWED_ORIGINS.split(",")
+  : ["http://localhost:3000"]; // Default to localhost if not set
+
+console.log("Allowed Origins:", allowedOrigins); // Log for debugging
+
+// HÄMTA ALLOWED ORIGINS FRÅN ENV.
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      // no clutter in prod mode.
+      if (process.env.NODE_ENV === "development") {
+        console.error(`CORS Error: Origin ${origin} is not allowed`); // DEBUG ORIGIN, VAR DEN FAILAR.
+      }
+      callback(new Error("Not allowed by CORS")); // Generic error message for the client.
+    }
+  },
+  credentials: true,
+};
 
 // Middlewares
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions)); // MTTODO: CORS middleware
 app.use(cookieParser());
 
 // Routes
