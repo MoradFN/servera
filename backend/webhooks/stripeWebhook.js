0f22ff276dab52ed3db1b6a272a8e14309ca39aa
MoradFN <71790104+MoradFN@users.noreply.github.com>
2024-12-10 06:28:43 +0100

    test mode 2

--- backend/webhooks/stripeWebhook.js
+++ backend/webhooks/stripeWebhook.js
@@ -1,19 +1,34 @@
 export const stripeEvents = async (event) => {
   const { type, data } = event;
-  const subscription = data.object;
+  const eventData = data.object; // Correct variable usage
 
   switch (type) {
     case "customer.subscription.created":
-      //   console.log(`Subscription Created: ${subscription.id}`);
+      console.log(`Subscription Created: ${eventData.id}`);
+      console.log("EVENT DATA: customer.subscription.created", eventData);
       break;
+
+    case "customer.subscription.updated":
+      console.log(`Subscription Updated: ${eventData.id}`);
+      console.log("EVENT DATA: customer.subscription.updated", eventData);
+      break;
+
     case "invoice.payment_succeeded":
-      //   console.log(`Payment Succeeded: ${subscription.subscription}`);
+      console.log(
+        `Payment Succeeded for Subscription: ${eventData.subscription}`
+      );
+      console.log("EVENT DATA: invoice.payment_succeeded", eventData);
       break;
+
     case "invoice.payment_failed":
-      //   console.error(`Payment Failed: ${subscription.subscription}`);
+      console.error(
+        `Payment Failed for Subscription: ${eventData.subscription}`
+      );
+      console.error("EVENT DATA: invoice.payment_failed", eventData);
       break;
+
     default:
-      console.log(type);
-    //   console.log(`Unhandled Event Type: ${type}`);
+      console.log(`(WEBHOOK DEFAULT): Unhandled Event Type: ${type}`);
+      console.log("(WEBHOOK DEFAULT): UNHANDELED EVENT DATA:", eventData);
   }
 };
