8ed5d627c226df746d7abcaeafd92f47914f95e5
MoradFN <71790104+MoradFN@users.noreply.github.com>
2024-12-11 08:50:52 +0100

    HEro

---
+++ frontend/src/components/AppHero.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { RouterLink } from "vue-router";
+import { defineProps } from "vue";
+
+defineProps({
+  title: {
+    type: String,
+    default: "Become a Vue Dev",
+  },
+  subtitle: {
+    type: String,
+    default: "Find the Vue job that fits your skills and needs",
+  },
+});
+</script>
+<template>
+  <section class="bg-blue-400 py-20 mb-4">
+    <div
+      class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center"
+    >
+      <div class="text-center">
+        <h1 class="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">
+          Din Restaurang, Din Stil
+        </h1>
+        <p class="my-4 text-xl text-white">
+          Anpassar din restaurang på dina vilkor <br />
+          Skapa dina egna unika menyer och anpassade teman. <br />
+          Ta kontroll över din restautang och effektivisera din verksamhet idag!
+        </p>
+      </div>
+      <div class="heroNavWrapper">
+        <nav>
+          <RouterLink to="/get-started">[Kom igång]</RouterLink>
+          <RouterLink to="/about">[Läs mer]</RouterLink>
+        </nav>
+      </div>
+    </div>
+  </section>
+</template>
--- frontend/src/views/AppHome.vue
+++ frontend/src/views/AppHome.vue
@@ -1,39 +1,11 @@
 <script setup>
-import { ref, onMounted } from "vue";
 import TheWelcome from "../components/TheWelcome.vue";
-
-const apiData = ref(null);
-
-onMounted(async () => {
-  try {
-    // Use the environment variable for the API base URL
-    const API_URL = import.meta.env.VITE_API_BASE_URL;
-    const response = await fetch(`${API_URL}/api/test`);
-
-    if (!response.ok) {
-      throw new Error(`Error: ${response.statusText}`);
-    }
-
-    apiData.value = await response.json();
-  } catch (error) {
-    console.error("Failed to fetch API data:", error);
-    apiData.value = { success: false, data: [] }; // Fallback in case of failure
-  }
-});
+import AppHero from "@/components/AppHero.vue";
 </script>
 
 <template>
   <main>
+    <AppHero />;
     <TheWelcome />
-    <div v-if="apiData && apiData.success">
-      <p>Success: {{ apiData.success }}</p>
-      <ul>
-        <li v-for="item in apiData.data" :key="item.id">
-          {{ item.name }} - {{ item.email }} - {{ item.subscription_status }}
-        </li>
-      </ul>
-    </div>
-    <p v-else-if="apiData">No data available.</p>
-    <p v-else>Loading...</p>
   </main>
 </template>
